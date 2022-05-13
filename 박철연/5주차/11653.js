d3efc7f81a032136600a27c312d2a33af97efa1a
oaat9309 <kite1993@naver.com>
2022-05-13 13:34:11 +0900

    5주차 문제풀이 완료!

--- 박철연/5주차/11653.js
+++ 박철연/5주차/11653.js
@@ -1,1 +1,61 @@
 // 소인수 분해
+
+// 첫째 줄에 정수 N (1 ≤ N ≤ 10,000,000)이 주어진다.
+// 정수 N이 주어졌을 때, 소인수분해하는 프로그램을 작성하시오.
+
+// let N = Math.ceil(Math.random() * 10000000);
+// let resultArr = [];
+// let finalArr = [];
+// let answer = "";
+
+// // 자기자신보다 작은 수로 나누고 그걸 또 나누고
+// for (let i = 1; i <= N; i++) {
+//   if (N % i === 0) {
+//     resultArr.push(i);
+//   }
+// }
+// console.log(resultArr);
+// let num = N;
+
+// while (num !== 1) {
+//   let smallNum;
+//   // 1이 언제나 resultArr의 첫번째 항목(resultArr[0])으로 들어가므로 i를 1부터 시작
+//   for (let i = 1; i < resultArr.length; i++) {
+//     if (num % resultArr[i] === 0) {
+//       smallNum = resultArr[i];
+//       break;
+//     }
+//   }
+//   console.log(smallNum);
+//   if (resultArr.length >= 4 && num > 1) {
+//     finalArr.push(smallNum);
+//     while (true) {
+//       num = num / smallNum;
+//       console.log("smallNum : " + smallNum);
+//       if (num % smallNum !== 0) {
+//         break;
+//       }
+//       finalArr.push(smallNum);
+//     }
+//   }
+// }
+
+// finalArr.forEach((numEl) => {
+//   answer += numEl + "\n";
+// });
+// console.log(answer);
+
+let answer = [];
+let N = Math.ceil(Math.random() * 100);
+console.log("숫자는 " + N);
+for (let i = 2; i <= N; i++) {
+  let num = i;
+  while (N % num === 0) {
+    answer.push(num);
+    if (N / num === 1) break;
+    // 여기가 중요! N을 이미 한번 거친 num으로 나누어서 새로운 N을 명시하는 것으로 num이 제곱 이상일 때를 나타냄
+    N = N / num;
+  }
+}
+
+console.log(answer.join("\n").trim());
--- 박철연/5주차/11792.js
+++ 박철연/5주차/11792.js
@@ -1,1 +1,57 @@
 // 하노이 탑 이동순서
+
+// 로직 도식화 (괄호 안 숫자는 기둥입니다.)
+
+// 1. 원반 1을 (3)로
+// 2. 원반 2를 (2)으로
+// 3. 원반 1을 (2)로 ((2):1-2)
+// 4. 원반 3을 (3)으로 ((2):1-2 & (3):3)
+// 5. 원반 1을 (1)로 ((1):1-4-5 & (2):2 & (3):3)
+// 6. 원반 2를 (3)으로 ((1):1-4-5 & (3):2-3)
+// 7. 원반 1을 (3)으로 ((1):4-5 & (3):1-2-3)
+// 8. 원반 4를 (2)로 ((1):5 & (2):4 & (3):1-2-3)
+// 9. 원반 1을 (2)로 ((1):5 & (2):1-4 & (3):2-3)
+// 10. 원반 2를 (1)로 ((1):2-5 & (2):1-4 & (3):3)
+// 더러워서 포기!
+
+let N = Math.ceil(Math.random() * 20);
+
+let towers = [[], [], []];
+let moveCount = 0;
+let output = [];
+
+const hanoi = (num) => {
+  // init
+  for (let i = 0; i < num; i++) {
+    towers[0].push(i);
+  }
+
+  //num개를 0부터 2까지 옮겨라
+  moveVia(num, 0, 2, 1);
+
+  console.log(moveCount);
+  console.log(output.join("\n"));
+};
+
+const move = (from, to) => {
+  const tmp = towers[from].pop();
+  towers[to].push(tmp);
+  moveCount++;
+  output.push(`${from + 1} ${to + 1}`);
+  return;
+};
+
+const moveVia = (num, from, to, via) => {
+  if (num == 1) {
+    return move(from, to);
+  }
+
+  moveVia(num - 1, from, via, to);
+  move(from, to);
+  moveVia(num - 1, via, to, from);
+};
+
+console.log(N);
+hanoi(N);
+
+// https://chunghyup.tistory.com/69
