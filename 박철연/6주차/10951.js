a3c37c0367f06d65fd0d20bd587f83c9099b6e21
oaat9309 <kite1993@naver.com>
2022-05-19 10:11:01 +0900

    6주차 문제풀이 수정

--- 박철연/6주차/10951.js
+++ 박철연/6주차/10951.js
@@ -1,16 +1,14 @@
+let arr = [];
 const fs = require("fs");
-const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+const input = fs.readFileSync("/dev/stdin").toString().split("\n");
+let i = 0;
 
-let length = input.length;
-let count = 0;
-
-while (length > 0) {
-  let num = input[count].split(" ").map(Number);
-  let sum = num[0] + num[1];
-  console.log(sum);
-
-  count++;
-  length--;
+while (input[i]) {
+  let arr = input[i].split(" ");
+  let a = parseInt(arr[0]);
+  let b = parseInt(arr[1]);
+  if (a > 0 && b > 0) {
+    console.log(a + b);
+  }
+  i++;
 }
-
-// while(true)로 무한 루프 만든 다음, length가 0이 될때 break 해줘도 되는데 시간은 더 오래 걸릴듯
